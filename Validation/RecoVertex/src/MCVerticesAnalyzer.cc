94aaa2080b8127f847f5fb76045bc02a0d53c5be
Andrea <andrea.venturi@cern.ch>
2013-11-12 11:59:26 +0100

    Sync my-53x to Validation/RecoVertex V00-13-05-05-02

--- Validation/RecoVertex/src/MCVerticesAnalyzer.cc
+++ Validation/RecoVertex/src/MCVerticesAnalyzer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Andrea Venturi
 //         Created:  Thu Dec 16 16:32:56 CEST 2010
-// $Id: MCVerticesAnalyzer.cc,v 1.6 2011/11/12 16:49:19 venturia Exp $
+// $Id: MCVerticesAnalyzer.cc,v 1.5 2011/11/12 09:41:59 venturia Exp $
 //
 //
 
@@ -77,7 +77,6 @@ private:
   edm::InputTag m_weight;
 
   TH1F* m_hnvtx;
-  TH2F* m_hnvtxvsbx;
   TH1F* m_hlumi;
   TH2F* m_hnvtxvslumi;
   TH1F* m_hnvtxweight;
@@ -116,10 +115,6 @@ MCVerticesAnalyzer::MCVerticesAnalyzer(const edm::ParameterSet& iConfig):
 
   m_hnvtx = tfserv->make<TH1F>("nvtx","Number of pileup vertices",60,-0.5,59.5);
   m_hnvtx->GetXaxis()->SetTitle("Number of Interactions");
-
-  m_hnvtxvsbx = tfserv->make<TH2F>("nvtxvsbx","Number of pileup vertices vs BX",9,-4.5,4.5,60,-0.5,59.5);
-  m_hnvtxvsbx->GetXaxis()->SetTitle("BX number");
-  m_hnvtxvsbx->GetYaxis()->SetTitle("Number of Interactions");
 
   m_hlumi = tfserv->make<TH1F>("lumi","BX luminosity*xsect",200,0.,50.);
   m_hlumi->GetXaxis()->SetTitle("Average Number of Interactions");
@@ -186,12 +181,6 @@ MCVerticesAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   // look for the intime PileupSummaryInfo
 
      std::vector<PileupSummaryInfo>::const_iterator pileupinfo;
-
-     for(pileupinfo = pileupinfos->begin(); pileupinfo != pileupinfos->end() ; ++pileupinfo) {
-       m_hnvtxvsbx->Fill(pileupinfo->getBunchCrossing(),pileupinfo->getPU_NumInteractions(),weight);
-     } 
-
-
      for(pileupinfo = pileupinfos->begin(); pileupinfo != pileupinfos->end() ; ++pileupinfo) {
        if(pileupinfo->getBunchCrossing()==0) break;
      } 
--- Validation/RecoVertex/test/bspvanalyzer_cfg.py
+++ Validation/RecoVertex/test/bspvanalyzer_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("MCvertices")
 
 #prepare options
 
-options = VarParsing.VarParsing("analysis")
+options = VarParsing.VarParsing()
 
 options.register ('globalTag',
                   "DONOTEXIST::All",
@@ -66,27 +66,43 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
                     fileNames = cms.untracked.vstring(),
-    
 #                    skipBadFiles = cms.untracked.bool(True),
                     inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
                     )
 
 
-process.source.fileNames = cms.untracked.vstring(options.inputFiles)
+process.source.fileNames = cms.untracked.vstring(
+"/store/relval/CMSSW_4_3_0_pre1/RelValZTT/GEN-SIM-RECO/MC_42_V7-v1/0053/1640096C-9E59-E011-BBE5-001A92971B8A.root",
+"/store/relval/CMSSW_4_3_0_pre1/RelValZTT/GEN-SIM-RECO/MC_42_V7-v1/0048/F0BF09EC-0559-E011-AFD4-0018F3D0966C.root",
+"/store/relval/CMSSW_4_3_0_pre1/RelValZTT/GEN-SIM-RECO/MC_42_V7-v1/0048/C85057F9-0A59-E011-936D-0030486792B6.root"
+)
+
+process.load("Validation.RecoVertex.bspvanalyzer_cfi")
+process.bspvanalyzer.pvCollection = cms.InputTag("goodVertices")
+process.bspvanalyzer.bspvHistogramMakerPSet.histoParameters = cms.untracked.PSet(
+    nBinX = cms.untracked.uint32(2000), xMin=cms.untracked.double(-0.2), xMax=cms.untracked.double(0.2),
+    nBinY = cms.untracked.uint32(2000), yMin=cms.untracked.double(-0.2), yMax=cms.untracked.double(0.2),
+    nBinZ = cms.untracked.uint32(200), zMin=cms.untracked.double(-30.), zMax=cms.untracked.double(30.),
+    nBinZProfile = cms.untracked.uint32(60), zMinProfile=cms.untracked.double(-30.), zMaxProfile=cms.untracked.double(30.)
+    )
+process.bspvanalyzer.bspvHistogramMakerPSet.runHisto = cms.untracked.bool(False)
+
+process.bspvnoslope = process.bspvanalyzer.clone()
+process.bspvnoslope.bspvHistogramMakerPSet.useSlope = cms.bool(False)
 
 
-process.load("Validation.RecoVertex.mcverticesanalyzer_cfi")
+process.load("Validation.RecoVertex.pvSelectionSequence_cff")
 
 
-process.p626 = cms.Path(process.mcverticesanalyzer) 
+process.p0 = cms.Path(process.goodVertices + process.bspvanalyzer + process.bspvnoslope)
 
 #----GlobalTag ------------------------
 
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = options.globalTag
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = options.globalTag
 
 
 process.TFileService = cms.Service('TFileService',
-                                   fileName = cms.string('mcvertices.root')
+                                   fileName = cms.string('bspvanalyzer.root')
                                    )
 
