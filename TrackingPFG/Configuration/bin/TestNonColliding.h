5b7c9165f4aa9605d6411a13960e456730a6fba2
Andrea Venturi <andrea.venturi@cern.ch>
2014-01-10 14:00:44 +0100

    New root macros added to my personal code

---
+++ TrackingPFG/Configuration/bin/TestNonColliding.h
@@ -0,0 +1,6 @@
+#ifndef TrackingPFG_Configuration_TestNonColliding_h
+#define TrackingPFG_Configuration_TestNonColliding_h
+
+void TestNonColliding(const char* fullname, const char* postfix);
+
+#endif // TrackingPFG_Configuration_TestNonColliding_h
