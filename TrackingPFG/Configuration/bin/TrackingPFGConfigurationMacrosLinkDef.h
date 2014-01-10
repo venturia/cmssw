c91d90d4310ec911ef2c54bd7f485c04db8bdce5
Andrea Venturi <andrea.venturi@cern.ch>
2014-01-10 14:00:44 +0100

    New root macros added to my personal code

---
+++ TrackingPFG/Configuration/bin/TrackingPFGConfigurationMacrosLinkDef.h
@@ -0,0 +1,8 @@
+#include "PVEffPlots.h"
+#include "TestNonColliding.h"
+#ifdef __CINT__
+#pragma link off all functions;
+#pragma link C++ function compareEfficiencies;
+#pragma link C++ function PVEffPlots;
+#pragma link C++ function TestNonColliding;
+#endif
--- TrackingPFG/Utilities/bin/TrackingPFGUtilitiesMacrosLinkDef.h
+++ TrackingPFG/Utilities/bin/TrackingPFGUtilitiesMacrosLinkDef.h
@@ -1,4 +1,5 @@
 #include "LogErrorFractionPlots.h"
+#include "PixelLessPlots.h"
 #ifdef __CINT__
 #pragma link off all functions;
 #pragma link C++ function genericErrorFractions;
@@ -7,4 +8,6 @@
 #pragma link C++ function errorFractionsVsAveNpileup;
 #pragma link C++ function errorFractionsVsNpixelclus;
 #pragma link C++ function errorFractionsVsNstripclus;
+#pragma link C++ function PixelLessPlotsForWeb;
+#pragma link C++ function PixelLessPlots;
 #endif
